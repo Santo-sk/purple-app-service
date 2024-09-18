a03182675f1ce50c8baad3b2f0edf42a0895fb70
Santo-sk <santhoshkanna98@gmail.com>
2024-09-18 17:57:21 +0530

    Initial setup for the spring boot application

---
+++ src/main/java/com/purple/pw_service/PwServiceApplication.java
@@ -0,0 +1,13 @@
+package com.purple.pw_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/purple/pw_service/PwServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purple.pw_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
